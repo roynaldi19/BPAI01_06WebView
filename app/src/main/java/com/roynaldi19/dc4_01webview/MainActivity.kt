d5146df558938374dcc943a5ff9032a46a0b0eac
roynaldi19 <roynaldi19@gmail.com>
2023-08-16 22:11:30 +0700

    update

--- app/src/main/java/com/roynaldi19/dc4_01webview/MainActivity.kt
+++ app/src/main/java/com/roynaldi19/dc4_01webview/MainActivity.kt
@@ -1,35 +1,42 @@
 package com.roynaldi19.dc4_01webview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.roynaldi19.dc4_01webview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val webView = findViewById<WebView>(R.id.webView)
-        webView.settings.javaScriptEnabled = true
+        binding.webView.settings.javaScriptEnabled = true
 
-        webView.webViewClient = object : WebViewClient() {
+        binding.webView.webViewClient = object : WebViewClient() {
             override fun onPageFinished(view: WebView, url: String) {
-                view.loadUrl("javascript:alert('Web Dicoding berhasil dimuat')")
+                view.loadUrl("javascript:alert('Web berhasil dimuat')")
             }
         }
 
-        webView.webChromeClient = object : WebChromeClient() {
-            override fun onJsAlert(view: WebView, url: String, message: String, result: android.webkit.JsResult): Boolean {
+        binding.webView.webChromeClient = object : WebChromeClient() {
+            override fun onJsAlert(
+                view: WebView,
+                url: String,
+                message: String,
+                result: android.webkit.JsResult
+            ): Boolean {
                 Toast.makeText(this@MainActivity, message, Toast.LENGTH_LONG).show()
                 result.confirm()
                 return true
             }
         }
 
-        webView.loadUrl("https://www.dicoding.com")
+        binding.webView.loadUrl("https://www.github.com/roynaldi19")
     }
 }
